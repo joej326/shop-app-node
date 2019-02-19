54c265715b26a41017be886cfa244707e966b361
Joseph Joyce <joej326@gmail.com>
2019-02-19 08:45:52 -0500

    push mac

--- models/product.js
+++ models/product.js
@@ -3,6 +3,17 @@ const path = require('path');
 
 const productsPath = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
 
+const getProductsFromFile = (callback) => {
+    fs.readFile(productsPath, (err, fileContent) => {
+        if (err) {
+            return callback([]) // had to add a callback to deal with asychronous code. We were getting length of undefined before.
+        } else {
+            callback(JSON.parse(fileContent));
+        }
+    })
+    // return products;
+};
+
 
 module.exports = class Product {
     constructor(t) {
@@ -11,30 +22,23 @@ module.exports = class Product {
     
 
     save() {
-
-        fs.readFile(productsPath, (err, fileContent) => {
-            let products = [];
-            // err will occur is no products (on first run)
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile((products) => {
             products.push(this); // "this" refers to the class ^^^^
             fs.writeFile(productsPath, JSON.stringify(products), err => {
                 console.log('ERROR:', err);
             });
         });
+
+        fs.readFile(productsPath, (err, fileContent) => {
+            let products = [];
+            
+            
+        });
     }
 
     // static makes it so it will always be called from this class and not other instances.
     static fetchAll(callback) {
-        fs.readFile(productsPath, (err, fileContent) => {
-            if (err) {
-                callback([]) // had to add a callback to deal with asychronous code. We were getting length of undefined before.
-            } else {
-                callback(JSON.parse(fileContent));
-            }
-        })
-        // return products;
+        getProductsFromFile(callback);
     }
 
 };
